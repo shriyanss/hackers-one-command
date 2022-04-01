cc77702c672188f2ebc2755b840e971ac73a2e8b
Shriyans Sudhi <71912662+shriyanss@users.noreply.github.com>
2022-04-01 07:50:00 +0000

    Create ipResolve.py

--- utils/ipResolve.py
+++ utils/ipResolve.py
@@ -1,8 +1,8 @@
 with open("subdomains.txt", 'r') as file:
-  for line in file:
-    try:
-      line = line.replace("\n", '')
-      ip = socket.gethostbyname(line)
-      print(line + ": " + ip)
-     except:
-       pass
+	for line in file:
+		line = line.replace("\n", '')
+		try:
+			ip = socket.gethostbyname(line)
+			print(f"{line}: {ip}")
+		except:
+			pass
